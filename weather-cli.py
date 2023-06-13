358c9cb7d6e8c7d306bd80432dff195ec33b176f
Pranav Puri <pranavpuri@kpmg.com>
2023-06-13 17:38:16 +0530

    fixing script

--- weather-cli.py
+++ weather-cli.py
@@ -2,20 +2,28 @@ import requests
 import sys
 import json
 import argparse
+import os
+from dotenv import load_dotenv
 
-def get_weather(city):
-    api_key = "YOUR_API_KEY"
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {"q": city, "appid": api_key, "units": "metric"}
+load_dotenv()
 
+API_KEY = os.getenv("API_KEY")
+
+
+BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+
+def get_weather(lat: float, long: float) -> dict:
+    params = {"lat": lat, "lon": long, "appid": API_KEY, "units": "metric"}
+    url = f"{BASE_URL}/weather"
     try:
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        data = response.json()
-        return data
+        req = requests.get(url, params=params)
+        req.raise_for_status()
+        res = req.json()
+        return res
     except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+        raise Exception(f"Error: {e}")
+
 
 def parse_weather_data(data):
     city = data["name"]
@@ -23,14 +31,18 @@ def parse_weather_data(data):
     description = data["weather"][0]["description"]
     return f"{city}: {temp}°C, {description}"
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Get weather information for a city.")
-    parser.add_argument("city", help="City name to get the weather forecast.")
-    return parser.parse_args()
+
+# def parse_arguments():
+#     parser = argparse.ArgumentParser(description="Get weather information for a city.")
+#     parser.add_argument("city", help="City name to get the weather forecast.")
+#     return parser.parse_args()
+
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    city_name = args.city
-    weather_data = get_weather(city_name)
+    # args = parse_arguments()
+    # city_name = args.city
+    lat = 28.7041
+    long = 77.1025
+    weather_data = get_weather(lat, long)
     weather_report = parse_weather_data(weather_data)
     print(weather_report)
